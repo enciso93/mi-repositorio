056853402eb96fc5d832fd021c2f7aa5f2a21c1e
hector enciso <hectordavid1210@gmail.com>
2025-07-09 15:53:47 -0500

    condicionales

---
+++ visual.py/ejemplo.py
@@ -0,0 +1,17 @@
+# numero1 = int(input("ingrese un numero"))
+# if numero > 0:
+#     print(f"positivo porque es {numero1}")
+# elif numero1 < 0:
+#     print(f"es negativo porque es {numero1}")
+# else:
+#     print(f"es cero{numero1}")
+
+# calcula el mayor de dos numeros ingresados
+numero1 = float(input("ingrese el primer numero"))
+numero2 = float(input("ingrese el segundo numero"))
+
+if numero1 > numero2:
+    print(f"el numero {numero1} es mayor que {numero2}")
+
+elif numero2 > numero1:
+    print(f"el numero {numero2} es mayor que {numero1}")
