403b80d60614344e90c14dcaa0dc5ef7395926f4
hector enciso <hectordavid1210@gmail.com>
2025-07-09 17:35:15 -0500

    condiconales

---
+++ visual.py/condicionales.py
@@ -0,0 +1,50 @@
+# talller condiconales
+# 1
+# numero = float(input(f"ingrese un numero:"))
+# if numero > 0:
+#     print(f"el numero {numero}es positivo")
+# elif numero < 0:
+#     print(f"el numero {numero} es negativo")
+# else:
+#     print(f"el numero es 0")
+
+# 2
+# numero1 = float(input("ingrese el primer numero"))
+# numero2 = float(input("ingrese el segundo numero"))
+
+# if numero1 > numero2:
+#      print(f"el numero {numero1} es mayor que {numero2}")
+
+# elif numero2 > numero1:
+#      print(f"el numero {numero2} es mayor que {numero1}")
+
+# 3
+# numero = float(input("ingrese un numero entero "))
+# if numero % 2 == 0:
+#     print(f"el numero {numero} es par")
+# else:
+#     print(f"el numero {numero} es impar")
+
+# 4
+# numero = float(input(f"ingresa un numero: "))
+# if 10<= numero <= 20:
+#     print(f"el numero {numero} esta entre 10 y 20")
+# else:
+#     print(f"el numero {numero} no esta entre 10 y 20")    
+
+# 5
+# numero = [float(input(f"ingrese el primer numero: ")) , float(input(f"ingrese el segundo numero: ")) , float(input(f"ingresa el tercer numero: "))] 
+# if numero [0] > numero[1] and numero[0] > numero[2]:
+#     print(f"el numero {numero[0]} es mayor que {numero[1]} y {numero[2]}")
+# elif numero [1] > numero [0] and numero [2]:
+#     print(f"el numero {numero[1]} es mayor que {numero[0]} y {numero[2]}")
+# else:
+#     print(f"el numero {numero[2]} es mayor que{numero[0]} y {numero[1]}")
+
+# 6
+ precio = float(inputfungresa el precio de un producto: "))
+ precio precio (precio*0.1)
+ if precio 100: 
+     print(f"El precio final del producto con 10% de descuento es {prec10}")
+ else:
+     print(f"El precio final sin el 10% de descuento es: {prec}")
