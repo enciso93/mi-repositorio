2171096dbd89a2143338a5458585064c53f77739
hector enciso <hectordavid1210@gmail.com>
2025-07-28 17:32:11 -0500

    taller2trim

--- visual.py/ejemplo.py
+++ visual.py/ejemplo.py
@@ -7,11 +7,23 @@
 #     print(f"es cero{numero1}")
 
 # calcula el mayor de dos numeros ingresados
-numero1 = float(input("ingrese el primer numero"))
-numero2 = float(input("ingrese el segundo numero"))
+# numero1 = float(input("ingrese el primer numero"))
+# numero2 = float(input("ingrese el segundo numero"))
 
-if numero1 > numero2:
-    print(f"el numero {numero1} es mayor que {numero2}")
+# if numero1 > numero2:
+#     print(f"el numero {numero1} es mayor que {numero2}")
 
-elif numero2 > numero1:
-    print(f"el numero {numero2} es mayor que {numero1}")
+# elif numero2 > numero1:
+#     print(f"el numero {numero2} es mayor que {numero1}")
+
+
+# 3 nombres y el monto que cada unog asta y promedio
+
+paco = float(input(f"cual fue el monto de paco"))
+luis = float(input(f"cual fue el monto de luis"))
+gerardo = float(input(f"cual fue el monto de gerardo"))
+
+tupla = ("paco", "luis", "gerardo")
+tot = paco + luis + gerardo
+promedio = tot /3
+print(tot, promedio)
---
+++ visual.py/mayus.py
@@ -0,0 +1,5 @@
+A = input("ingresa una letra")
+if A == "b" or A == "h" or A =="z" or A =="t":
+    print(f"su letra en mayuscula es: {A.upper()}")
+else:
+    print(f"la letra no es valida")
